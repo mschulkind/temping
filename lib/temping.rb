6cf7b85e5aa67fc7b2a74bf949f2ad8d4b106638
Matthew Schulkind <mschulkind@gmail.com>
2013-12-06 00:42:05 -0500

    Remove the reuse of already defined constants.

--- lib/temping.rb
+++ lib/temping.rb
@@ -4,39 +4,41 @@ require "active_support/core_ext/string"
 class Temping
   def self.create(model_name, &block)
     factory = ModelFactory.new(model_name.to_s.classify, &block)
-    factory.klass
+    factory.build
   end
 
   class ModelFactory
     def initialize(model_name, &block)
       @model_name = model_name
-      klass.class_eval(&block) if block_given?
+      @block = block
     end
-
-    def klass
-      @klass ||= Object.const_get(@model_name)
-    rescue NameError
-      @klass = build
-    end
-
-    private
 
     def build
       Class.new(ActiveRecord::Base).tap do |klass|
+        @klass = klass
+
+        if Object.const_defined?(@model_name)
+          Object.send(:remove_const, @model_name)
+        end
+
         Object.const_set(@model_name, klass)
 
         klass.primary_key = :id
         create_table
         add_methods
+
+        klass.class_eval(&@block) if @block
       end
     end
+
+    private
 
     def create_table
       connection.create_table(table_name, :temporary => true)
     end
 
     def add_methods
-      class << klass
+      class << @klass
         def with_columns
           connection.change_table(table_name) do |table|
             yield(table)
@@ -50,11 +52,11 @@ class Temping
     end
 
     def connection
-      klass.connection
+      @klass.connection
     end
 
     def table_name
-      klass.table_name
+      @klass.table_name
     end
   end
 end
